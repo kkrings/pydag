641f414c4d33c5b25c8ee69b2b0f257ea50f00e2
kkrings <kai.krings88@gmx.net>
2016-07-07 11:09:27 +0200

    Added setup.py to project.

---
+++ setup.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import setuptools
+
+
+setuptools.setup(
+    name="PyDAG",
+    version="0.1",
+    author="Kai Krings",
+    author_email="kai.krings88@gmx.net",
+    description="Simple Python interface to HTCondor/DAGMan",
+    url="https://github.com/kkrings/pydag/",
+    license="GPLv3",
+    packages=setuptools.find_packages())
