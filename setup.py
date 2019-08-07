b70f2de9e2d5dac7a4c3ebc94d22d91981d2490a
Kai Krings <kai.krings@posteo.de>
2019-08-07 16:36:02 +0200

    Fix e-mail address.

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setuptools.setup(
     name="pydag",
     version="0.1",
     author="Kai Krings",
-    author_email="kai.krings@posteo.net",
+    author_email="kai.krings@posteo.de",
     description="Simple Python interface to HTCondor/DAGMan",
     url="https://github.com/kkrings/pydag/",
     license="GPLv3",
